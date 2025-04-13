51f8fb0719d119e2ba57481256d1adf33a977d90
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-04-13 16:37:31 +0200

    Add doc

--- Utils.pm
+++ Utils.pm
@@ -53,3 +53,121 @@ sub material_type {
 1;
 
 __END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+MARC::Leader::Utils - Utilities for MARC::Leader.
+
+=head1 SYNOPSIS
+
+ use MARC::Leader::Utils qw(material_type);
+
+ my $material_type = material_type($leader_obj);
+
+=head1 SUBROUTINES
+
+=head2 C<material_type>
+
+ my $material_type = material_type($leader_obj);
+
+Get material type.
+This process is defined in MARC 008 field.
+
+C<$leader_obj> variable is L<Data::MARC::Leader> instace.
+
+Returned strings are:
+
+=over 8
+
+=item * book
+=item * computer_file
+=item * continuing_resource
+=item * map
+=item * mixed_material
+=item * music
+=item * visual_material
+
+=back
+
+Returns string.
+
+=head1 ERRORS
+
+ material_type():
+         Leader object must be a Data::MARC::Leader instance.
+         Unsupported material type.
+
+=head1 EXAMPLE
+
+=for comment filename=material_type.pl
+
+ use strict;
+ use warnings;
+
+ use MARC::Leader;
+ use MARC::Leader::Utils qw(material_type);
+
+ if (@ARGV < 1) {
+         print STDERR "Usage: $0 leader_string\n";
+         exit 1;
+ }
+ my $leader_string = $ARGV[0];
+
+ my $leader = MARC::Leader->new->parse($leader_string);
+
+ my $material_type = material_type($leader);
+
+ print "Leader: |$leader_string|\n";
+ print "Material type: $material_type\n";
+
+ # Output for '     nem a22     2  4500':
+ # Leader: |     nem a22     2  4500|
+ # Material type: map
+
+=head1 DEPENDENCIES
+
+L<Error::Pure>
+L<Exporter>
+L<File::Spec::Functions>,
+L<File::Share>,
+L<Readonly>.
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Data::MARC::Leader>
+
+Data object for MARC leader.
+
+=item L<MARC::Leader>
+
+MARC leader class.
+
+=back
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/MARC-Leader-Utils>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© 2025 Michal Josef Špaček
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut

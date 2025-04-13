d79da8f263d8205a8d233d64c6669b063efc9c7c
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-04-13 16:24:31 +0200

    Implement material leader

--- Utils.pm
+++ Utils.pm
@@ -5,6 +5,7 @@ use strict;
 use warnings;
 
 use Error::Pure qw(err);
+use List::Util 1.33 qw(any);
 use Scalar::Util qw(blessed);
 use Readonly;
 
@@ -23,7 +24,30 @@ sub material_type {
 		err "Leader object must be a Data::MARC::Leader instance.";
 	}
 
-	# TODO
+	my $material_type;
+	if ((any { $leader->type eq $_ } qw(a t))
+		&& (any { $leader->bibliographic_level eq $_ } qw(a c d m))) {
+
+		$material_type = 'book';
+	} elsif ($leader->type eq 'm') {
+		$material_type = 'computer_file';
+	} elsif (any { $leader->type eq $_ } qw(e f)) {
+		$material_type = 'map';
+	} elsif (any { $leader->type eq $_ } qw(c d i j)) {
+		$material_type = 'music';
+	} elsif ($leader->type eq 'a'
+		&& (any { $leader->bibliographic_level eq $_ } qw(b i s))) {
+
+		$material_type = 'continuing_resource';
+	} elsif (any { $leader->type eq $_ } qw(g k o r)) {
+		$material_type = 'visual_material';
+	} elsif ($leader->type eq 'p') {
+		$material_type = 'mixed_material';
+	} else {
+		err "Unsupported 008 type.";
+	}
+
+	return $material_type;
 }
 
 1;
